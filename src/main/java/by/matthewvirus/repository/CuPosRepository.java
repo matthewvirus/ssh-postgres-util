cf2066e0fffccf792ccf528cf723318c616f7b0a
Matthew Zhigalo <matthewzhigalo@email.com>
2025-02-04 10:33:22 +0300

    Created DB repository classes

---
+++ src/main/java/by/matthewvirus/repository/CuPosRepository.java
@@ -0,0 +1,170 @@
+package by.matthewvirus.repository;
+
+import by.matthewvirus.model.DefaultValues;
+import by.matthewvirus.model.json.CuPos;
+import by.matthewvirus.model.json.Totals;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CuPosRepository {
+
+    private final Connection conn;
+    private final int lentaId;
+    private final boolean isShiftClosed;
+    private static final String isShiftClosedSQL = "select * from cash_voucher where lenta_id=? and voucher_type=6";
+    private static final String shiftOpenTSQL = "select doc_date as open_t from cash_voucher where lenta_id=? and voucher_type=0";
+    private static final String shiftCloseTSQL = "select doc_date close_t from cash_voucher where lenta_id=? and voucher_type=6";
+    private static final String shiftCloseTIsntClosedSQL = "select doc_date as close_t from cash_voucher where skno_number=(select max(skno_number) from cash_voucher where lenta_id=?)";
+    private static final String shiftUIDSQL = "select doc_uid as shift_uid from cash_voucher where lenta_id=? and voucher_type=6";
+    private static final String lastDocNumSQL = "select max(skno_number) as last_doc from cash_voucher where lenta_id=?";
+    private static final String lastDocStatusSQL = "select status as status from cash_voucher where skno_number=(select max(skno_number) from cash_voucher where lenta_id=?)";
+    private static final String lastDocUIDSQL = "select doc_uid as doc_uid from cash_voucher where skno_number=(select max(skno_number) from cash_voucher where lenta_id=?)";
+    private static final String totalPDSCountSQL = "select count(*) as total_PD_count from cash_voucher where lenta_id=? and voucher_type=1";
+
+    public CuPosRepository(Connection conn, int lentaId) {
+        this.conn = conn;
+        this.lentaId = lentaId;
+        isShiftClosed = checkIfShiftClosed(conn, lentaId);
+    }
+
+    private String getShiftOpenTime() {
+        String dateTime = "null";
+        try (PreparedStatement statement = conn.prepareStatement(shiftOpenTSQL)) {
+            statement.setInt(1, lentaId);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                dateTime = resultSet.getString(1);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return dateTime;
+    }
+
+    private String getShiftCloseTime() {
+        String dateTime = "null";
+        try (PreparedStatement statement = isShiftClosed ? conn.prepareStatement(shiftCloseTSQL) : conn.prepareStatement(shiftCloseTIsntClosedSQL)) {
+            statement.setInt(1, lentaId);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                dateTime = resultSet.getString(1);
+            }
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return dateTime;
+    }
+
+    private String getShiftUID() {
+        String shiftUID = " ";
+        if (isShiftClosed) {
+            try (PreparedStatement statement = conn.prepareStatement(shiftUIDSQL)) {
+                statement.setInt(1, lentaId);
+                ResultSet resultSet = statement.executeQuery();
+                while (resultSet.next()) {
+                    shiftUID = resultSet.getString(1);
+                }
+                resultSet.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return shiftUID;
+    }
+
+    private long getLastDocNum() {
+        long lastDocNum = 0L;
+        try (PreparedStatement statement = conn.prepareStatement(lastDocNumSQL)) {
+            statement.setInt(1, lentaId);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                lastDocNum = resultSet.getLong(1);
+            }
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return lastDocNum;
+    }
+
+    private int getLastDocStatus() {
+        int lastDocStatus = 0;
+        try (PreparedStatement statement = conn.prepareStatement(lastDocStatusSQL)) {
+            statement.setInt(1, lentaId);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                lastDocStatus = resultSet.getInt(1);
+            }
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return (lastDocStatus == 1) ? 3 : 1;
+    }
+
+    private String getLastDocUID() {
+        String lastDocUID = " ";
+        try (PreparedStatement statement = conn.prepareStatement(lastDocUIDSQL)) {
+            statement.setInt(1, lentaId);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                lastDocUID = resultSet.getString(1);
+            }
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return lastDocUID;
+    }
+
+    private long getTotalPDsCount() {
+        long totalPDsCount = 0L;
+        try (PreparedStatement statement = conn.prepareStatement(totalPDSCountSQL)) {
+            statement.setInt(1, lentaId);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                totalPDsCount = resultSet.getLong(1);
+            }
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return totalPDsCount;
+    }
+
+    private boolean checkIfShiftClosed(Connection conn, int lentaId) {
+        boolean answer;
+        try (PreparedStatement statement = conn.prepareStatement(isShiftClosedSQL)) {
+            statement.setInt(1, lentaId);
+            ResultSet resultSet = statement.executeQuery();
+            answer = resultSet.next();
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return answer;
+    }
+
+    public CuPos cuposBuilder() {
+        return new CuPos(
+                DefaultValues.OWNER_NAME.getCode(),
+                " ",
+                isShiftClosed ? lentaId : 0,
+                getShiftOpenTime(),
+                getShiftCloseTime(),
+                getShiftUID(),
+                isShiftClosed ? 3 : 1,
+                getLastDocNum(),
+                getLastDocStatus(),
+                getLastDocUID(),
+                getTotalPDsCount(),
+                "null",
+                "null",
+                new Totals()
+        );
+    }
+}
---
+++ src/main/java/by/matthewvirus/repository/NumericTotalsRepository.java
@@ -0,0 +1,72 @@
+package by.matthewvirus.repository;
+
+import by.matthewvirus.model.DefaultValues;
+import by.matthewvirus.model.json.Totals;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class NumericTotalsRepository {
+
+    private final Connection conn;
+    private final int lentaId;
+
+    public static final String sumOfSalesSQL = "select sum(oper_sum) as sum_sales from wdfly.session_journal where cash_session_id=(select id from wdfly.cash_session where session_number=?) and oper_sum is not null and oper_type='30'";
+    public static final String sumOfCashSQL = "select sum(cash_sum) as cash_sum from wdfly.session_journal inner join wdfly.ticket on session_journal.ticket_id=ticket.id where cash_session_id=(select id from wdfly.cash_session where session_number=?) and fiscal=1 and oper_type='30' and cash_sum is not null";
+    public static final String sumOfNoncashSQL = "select sum(cashless_sum) as cashless_sum from wdfly.session_journal inner join wdfly.ticket on session_journal.ticket_id=ticket.id where cash_session_id=(select id from wdfly.cash_session where session_number=?) and fiscal=1 and oper_type='30' and cashless_sum is not null";
+    public static final String numOfAnnulSQL = "select count(oper_sum) as annul_count from wdfly.session_journal where cash_session_id=(select id from wdfly.cash_session where session_number=?) and session_journal.oper_type='65'";
+    public static final String sumOfAnnulSQL = "select sum(oper_sum) as annul_sum from wdfly.session_journal where cash_session_id=(select id from wdfly.cash_session where session_number=?) and session_journal.oper_type='65'";
+    public static final String numOfRefundsSQL = "select count(oper_sum) as refunds_count from wdfly.session_journal where cash_session_id=(select id from wdfly.cash_session where session_number=?) and session_journal.oper_type='60'";
+    public static final String sumOfRefundsSQL = "select sum(oper_sum) as refunds_sum from wdfly.session_journal where cash_session_id=(select id from wdfly.cash_session where session_number=?) and session_journal.oper_type='60'";
+    public static final String salesValueSQL = "select sum(oper_sum) as sales_value from wdfly.session_journal where cash_session_id=(select id from wdfly.cash_session where session_number=?) and oper_type='30' and oper_status=30";
+    public static final String numOfSalesSQL = "select count(*) as num_sales from wdfly.session_journal where cash_session_id=(select id from wdfly.cash_session where session_number=?) and oper_type='30'";
+    public static final String numOfCashInSQL = "select count(*) as num_cash_in from wdfly.session_journal where cash_session_id=(select id from wdfly.cash_session where session_number=?) and oper_type='80' and oper_status=30";
+    public static final String sumOfCashInSQL = "select sum(oper_sum) as sum_cash_in from wdfly.session_journal where cash_session_id=(select id from wdfly.cash_session where session_number=?) and oper_type='80' and oper_status=30";
+    public static final String sumOfCashOutSQL = "select sum(oper_sum) as sum_cash_out from wdfly.session_journal where cash_session_id=(select id from wdfly.cash_session where session_number=?) and oper_type='90' and oper_status=30";
+    public static final String numOfCashOutSQL = "select count(*) as num_cash_out from wdfly.session_journal where cash_session_id=(select id from wdfly.cash_session where session_number=?) and oper_type='90' and oper_status=30";
+
+    public NumericTotalsRepository(Connection conn, int lentaId) {
+        this.conn = conn;
+        this.lentaId = lentaId;
+    }
+
+    private Number queriesExecutor(String statementSql) {
+        Number data = 0;
+        try (PreparedStatement st = conn.prepareStatement(statementSql)){
+            st.setInt(1, lentaId);
+            ResultSet resultSet = st.executeQuery();
+            while (resultSet.next()) {
+                data = resultSet.getDouble(1);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return data;
+    }
+
+    public Totals totalsBuilder() {
+        return new Totals(
+                DefaultValues.CURRENCY_CODE.getCode(),
+                DefaultValues.CURRENCY_NAME.getCode(),
+                queriesExecutor(numOfSalesSQL).longValue(),
+                (long) (queriesExecutor(sumOfSalesSQL).doubleValue() * 100),
+                (long) (queriesExecutor(sumOfNoncashSQL).doubleValue() * 100),
+                (long) (queriesExecutor(sumOfCashSQL).doubleValue() * 100),
+                queriesExecutor(numOfRefundsSQL).longValue(),
+                (long) (queriesExecutor(sumOfRefundsSQL).doubleValue() * 100),
+                queriesExecutor(numOfCashInSQL).longValue(),
+                (long) (queriesExecutor(sumOfCashInSQL).doubleValue() * 100),
+                queriesExecutor(numOfCashOutSQL).longValue(),
+                (long) (queriesExecutor(sumOfCashOutSQL).doubleValue() * 100),
+                0,
+                0,
+                queriesExecutor(numOfAnnulSQL).longValue(),
+                (long) (queriesExecutor(sumOfAnnulSQL).doubleValue() * 100),
+                0,
+                0,
+                (long) (queriesExecutor(salesValueSQL).doubleValue() * 100)
+        );
+    }
+}
