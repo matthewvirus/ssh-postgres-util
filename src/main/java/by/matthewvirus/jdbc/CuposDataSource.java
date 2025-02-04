76e05fffb2f6e0013ff53c6eb26e2416a3eb3ced
Matthew Zhigalo <matthewzhigalo@email.com>
2025-02-04 11:18:51 +0300

    Refactored code

--- src/main/java/by/matthewvirus/jdbc/CuposDataSource.java
+++ src/main/java/by/matthewvirus/jdbc/CuposDataSource.java
@@ -2,43 +2,27 @@ package by.matthewvirus.jdbc;
 
 import javax.sql.DataSource;
 
-import by.matthewvirus.model.db_user.DbUserData;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
-import java.util.Objects;
 import java.util.Properties;
 import java.util.logging.Logger;
 
-@Getter
-@Setter
-public class CuposDataSource implements DataSource {
+public record CuposDataSource(String driver, String url, String name, String password) implements DataSource {
 
     private static volatile CuposDataSource instance;
-    private final String driver;
-    private final String url;
-    private final String name;
-    private final String password;
-
 
     public CuposDataSource(String driver, String url, String name, String password) {
         this.driver = driver;
         this.url = url;
         this.name = name;
         this.password = password;
-        instance = this;
-    }
-
-    public CuposDataSource(DbUserData dbUserData) {
-        this.driver = dbUserData.getDriverPath();
-        this.name = dbUserData.getUsername();
-        this.password = dbUserData.getPassword();
-        this.url = dbUserData.getCuposURL();
         instance = this;
     }
 
@@ -118,46 +102,4 @@ public class CuposDataSource implements DataSource {
     public boolean isWrapperFor(Class<?> iface) throws SQLException {
         throw new SQLException();
     }
-
-    public String driver() {
-        return driver;
-    }
-
-    public String url() {
-        return url;
-    }
-
-    public String name() {
-        return name;
-    }
-
-    public String password() {
-        return password;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (CuposDataSource) obj;
-        return Objects.equals(this.driver, that.driver) &&
-                Objects.equals(this.url, that.url) &&
-                Objects.equals(this.name, that.name) &&
-                Objects.equals(this.password, that.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(driver, url, name, password);
-    }
-
-    @Override
-    public String toString() {
-        return "CustomDataSource[" +
-                "driver=" + driver + ", " +
-                "url=" + url + ", " +
-                "name=" + name + ", " +
-                "password=" + password + ']';
-    }
-
 }
